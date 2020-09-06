faa730c6b511f3f4a4a87af378b244b0eafc0a9b
unknown <farentinus5111@gmail.com>
2020-09-06 22:42:56 +0700

    left navbar modif

--- resources/views/components/left-navbar.blade.php
+++ resources/views/components/left-navbar.blade.php
@@ -1,11 +1,12 @@
 <div class="h-100 border-0 custom-shadow custom-rounded-right overflow-auto">
 
 	@foreach($navigations as $navigation)
-
-	<div class="d-flex align-items-center flex-column py-3 left-item position-relative {{$isSelected($navigation['name'])}}">
-		<img src="{{ asset ($navigation['icon']) }}" alt="{{$navigation['name']}} Icon" width="35">
-		<span class="small">{{$navigation['name']}}</span>
-	</div>
+	<a href="/{{$navigation['name']}}" class="text-decoration-none">
+		<div class="d-flex align-items-center flex-column py-3 left-item position-relative {{$isSelected($navigation['name'])}}">
+			<img src="{{ asset ($navigation['icon']) }}" alt="{{$navigation['name']}} Icon" width="35">
+			<span class="small">{{$navigation['name']}}</span>
+		</div>
+	</a>
 
 	@endforeach
 
